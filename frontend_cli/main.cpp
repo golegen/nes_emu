d3afa21f511b0450c3000170a820f29dab9a5dc4
minary <min050820@gmail.com>
2016-12-05 00:14:47 +0900

    cli_frontend: rewrite command processor

--- frontend_cli/main.cpp
+++ frontend_cli/main.cpp
@@ -3,62 +3,67 @@
 #include <unistd.h>
 #include <chrono>
 #include <thread>
+#include <sstream>
 
 using namespace std;
 
 NES *nes;
 int scrwidth, scrheight;
 
+string toUpperCase(const string& str) {
+    string ret;
+    ret.resize(str.size());
+    for(int i=0; i<str.size(); i++)
+        if(97 <= str[i] && str[i] <= 122)
+            ret[i] = str[i] - (char)32;
+        else
+            ret[i] = str[i];
+    return ret;
+}
+
+string trim(string& str) {
+    size_t first = str.find_first_not_of(" \t");
+    size_t last = str.find_last_not_of(" \t");
+    return str.substr(first, (last-first+1));
+}
+
 void loop() {
-    nes->cpu->ins=nes->mmu->read(nes->cpu->PC());
-    nes->cpu->decode();
-    nes->printDebug("CPU", "[PC=$"+itos(nes->cpu->PC())+"] $"+itos(nes->mmu->read(nes->cpu->PC()))+" "+nes->cpu->disassembly(nes->cpu->PC()));
+    string nowline, command;
     while(true) {
         cout << "> ";
-        string str;
-        cin >> str;
-        if(str=="tick")
+        getline(cin, nowline);
+        istringstream stream(nowline);
+        stream >> command;
+        command = toUpperCase(command);
+        if(command == "TICK")
             nes->tick();
-        else if(str=="step" || str==".") {
-            for(int i=0; i<12; i++)
+        else if(command == "STEP") {
+            int steps = 12; // Default step; One CPU tick
+            stream >> steps;
+            for(int i=0; i<steps; i++)
                 nes->tick();
         }
-        else if(str=="start")
-            nes->start();
-        else if(str=="stop")
-            nes->stop();
-        else if(str=="mov-pc") {
-            int new_addr;
-            scanf("%x", &new_addr);
-            nes->cpu->PC(new_addr);
+        else if(command == "JMP" || command == "JUMP") {
+            uint16_t pc = nes->cpu->PC();
+            stream >> pc;
+            nes->cpu->PC(pc);
         }
-        else if(str=="push") {
-            int data;
-            scanf("%x", &data);
-            nes->cpu->push(data);
+        else if(command == "OPEN" || command == "LOAD") {
+            string filename;
+            getline(stream, filename);
+            filename = trim(filename);
+            nes->openFile(filename);
+            nes->hardReset();
         }
-        else if(str=="pop") {
-            printf("%02X", nes->cpu->peek());
-            nes->cpu->pop();
-        }
-        else if(str=="run") {
-            while(true) {
-                for(int i=0; i<12; i++)
-                    nes->tick();
-                if(nes->mmu->wroteAPU)
-                    break;
-                this_thread::sleep_for(250ms);
-            }
-        }
-        else if(str=="quit")
+        else if(command == "QUIT" || command == "EXIT")
             return;
+        else
+            clog << "Unrecognized command '" << command << '\'' << endl;
     }
 }
 
 int main(int argc, char** argv) {
     nes = new NES();
-    nes->openFile("/home/minary/nestest.nes");
-    nes->hardReset();
     loop();
     delete nes;
     return 0;
